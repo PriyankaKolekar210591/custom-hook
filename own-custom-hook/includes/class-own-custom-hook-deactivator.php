4ff0a877a3578f66a4114a010a93e952d615630a
priyanka1539 <priyanka.surve@carmatec.in>
2022-10-19 01:14:25 +0530

    Own Custom Hook Plugin Added

---
+++ own-custom-hook/includes/class-own-custom-hook-deactivator.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://#
+ * @since      1.0.0
+ *
+ * @package    Own_Custom_Hook
+ * @subpackage Own_Custom_Hook/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Own_Custom_Hook
+ * @subpackage Own_Custom_Hook/includes
+ * @author     Priyanka Kolekar <priyankasurve091@gmail.com>
+ */
+class Own_Custom_Hook_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
